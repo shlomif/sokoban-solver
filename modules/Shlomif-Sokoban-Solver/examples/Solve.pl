2abc9bfd5137700ba5e8ff9222eee9c3cde49316
(no author) <(no author)@ddf91b2d-1462-4000-aae5-8a08ddc4854f>
2007-12-29 16:35:34 +0000

    Added Solve.pl and its timing.

---
+++ modules/Shlomif-Sokoban-Solver/examples/Solve.pl
@@ -0,0 +1,22 @@
+use strict;
+use warnings;
+
+use Shlomif::Sokoban::Solver::Board;
+
+my $board_contents = <<'EOF';
+  ####
+  #  #
+  #  ####
+###$.$  #
+#  .@.  #
+#  $.$###
+####  #
+   #  #
+   ####
+EOF
+
+{
+    my $board = S->load($board_contents);
+
+    $board->solve();
+}
